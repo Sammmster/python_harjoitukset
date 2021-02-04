b430e245769666fca1a99be56bb02d8cd80e629f
Sami Tuominen <tuominen_s@hotmail.com>
2021-02-04 09:18:22 -0800

    lisättiin luokka (class) harjoitus

---
+++ harjoitus4.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+
+class Ensimmainen_luokka:
+    x=2
+# muuttujan x määrittely
+
+luokka_objekti = Ensimmainen_luokka
+print(luokka_objekti.x)
+# class hyvä tapa erottaa funktioista on aloittaa sen nimi isolla kirjaimella
+
+luokka_objekti.x = 4
+print(luokka_objekti.x)
