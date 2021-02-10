03393f6c99a9a1a00315f5876f9c641a3c00a1f6
Sami Tuominen <tuominen_s@hotmail.com>
2021-02-10 04:07:34 -0800

    Kuukaudet

--- Ohjelmointiputka/Osa5.py
+++ Ohjelmointiputka/Osa5.py
@@ -39,6 +39,42 @@ print("Toinen onkin", nimet[1])
 # Tässä vaihdetaan listan toinen nimi Uolevi Einariksi. Listan muuttujia voi muuttaa
 # kuten mitä muuta muuttujaa tahansa.
 
-print("Tässä vielä koko joukko:")
+# print("Tässä vielä koko joukko:", nimet)
+# jompikumpi tapa. Ylempi tulostaa koko joukon samalle riville lauseen kanssa.
+print ("Tässä vielä koko joukko:")
 print (nimet)
 
+## Ohjelman tulostus on seuraava:
+
+# Ensin tulee Henrikki
+# Sitten on Uolevi
+# Viimeisenä on Antti
+# Tässä vielä koko joukko:
+# ['Henrikki', 'Uolevi', 'Antti']
+# Toinen onkin Einari
+# Tässä vielä koko joukko:
+# ['Henrikki', 'Einari', 'Antti']
+
+## Listan luonissa alkiot merkitään hakasulkujen sisään pilkulla erotettuna. Tämän jälkeen alkioihin voi viitata
+## kokonaisluvuin nillasta alkaen: tässä nimet[0] on ensimmäinen nimi, nimet[1] on toinen nimi ja nimi[2]
+## on kolmas nimi. Listan koko sisällön voi tulostaa antamalla pelkän listan nimen print-komennolle.
+
+####################### esimerkki kuukaudet ######################
+## Yksi listan käyttötarkoitus on, että se toimii ohjelman tietovarastona. Seuraavassa ohjelmassa lista sisältää
+## kuukausien nimet suomeksi, jolloin ohjelman on helppo nimetä käyttäjän antama kuukausi.
+
+lista = ["tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu", "heinäkuu",
+        "elokuu", "syyskuu", "lokakuu", "marraskuu", "joulukuu"] 
+kuukausi = int(input("Anna kuukausi (1-12): "))
+if 1 <= kuukausi <= 12:
+    print("Kuukauden nimi:", lista[kuukausi - 1])
+else:
+    print("Kuukausi ei kelpaa!")
+
+## Ohjelman tulostus voi olla seuraava:
+## Anna kuukausi (1-12): 9
+## Kuukauden nimi syyskuu
+
+## Ohjelman toiminta perustuu siihen, että listan alkioon voi viitata muuttujan avulla. Listassa kauukausin
+## numerointi alkaa nollasta, minkä vuoksi ohjelma pienentää yhden käyttäjän antamaan numeroa. Esimerkiksi
+## jos käyttäjä antaa numeron 9, ohjelma hakee kuukauden nimen kohdasta lista[8].
