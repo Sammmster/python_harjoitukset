646fbafa22a72831f95f652b76106f67bc3ffb03
Sami Tuominen <tuominen_s@hotmail.com>
2021-02-10 05:09:44 -0800

    kolmiolaskuja

---
+++ 3s palautettava.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python3
+
+from math import sqrt
+# import ottaa käyttöön neliöjuuri funktion moduulista math.
+
+print("Haluatko laskea hypotenuusan(1) vai kateetin(2)?")
+vastaus = input("")
+if vastaus == "1":
+    kateetti1 = int(input("anna ensimmäinen kateetti "))
+    kateetti2 = int(input("anna toinen kateetti "))
+    hypotenuusa = sqrt(kateetti1**2 + kateetti2**2)
+    print ("hypotenuusan pituus on = " , hypotenuusa)
+## vastaus = "2"
+else: 
+    kateetti1 = int(input("anna ensimmäinen kateetti "))
+    hypotenuusa = int(input("anna hypotenuusan pituus "))
+    kateetti2 = sqrt(kateetti1**2 + hypotenuusa**2)
+    print ("toisen kateetin pituus on = " , kateetti2)
+print("Haluatko myös laskea kolmion pinta-alan (k/e)?")
+vastaus2 = input("")
+if vastaus2 == "k":
+    ala = sqrt(kateetti1*kateetti2/2)
+    print("Pinta-ala on ", ala)
+
+else:
+    print ("Mukavaa työpäivää")
