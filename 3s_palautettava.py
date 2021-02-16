ffe3b2b760e311cd81ec77ce87a1eddb1fc64e95
Sami Tuominen <tuominen_s@hotmail.com>
2021-02-16 06:13:50 -0800

    virheitä3

--- 3s_palautettava.py
+++ 3s_palautettava.py
@@ -14,7 +14,6 @@ if vastaus == "1":
 else: 
     kateetti1 = int(input("anna ensimmäinen kateetti "))
     hypotenuusa = int(input("anna hypotenuusan pituus "))
-    #kateetti2 = sqrt(kateetti1**2 + hypotenuusa**2)
     kateetti2 = sqrt(hypotenuusa**2 - kateetti1**2)
     print ("toisen kateetin pituus on = " , kateetti2)
 
