a62298768e5a9117cecca52d16618b67c6f42e6e
Sami Tuominen <tuominen_s@hotmail.com>
2021-02-16 04:12:55 -0800

    sd

---
+++ Ohjelmointiputka/python2020.py
@@ -0,0 +1,3 @@
+#! /usr/bin/python3
+# https://www.youtube.com/watch?v=kqtD5dpn9C8
+# Python Tutorial - Python for Beginners [2020]
---
+++ Ohjelmointiputka/python2021.py
@@ -0,0 +1,3 @@
+#! /usr/bin/python3
+# https://www.youtube.com/watch?v=LzYNWme1W6Q
+# Python Tutorial - Python for Beginners [2021]
